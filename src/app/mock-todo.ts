46a31bf80001f6e00873831d0d963ff5e8864c2f
Viacheslav_Valitskas <valitskas@gmail.com>
2016-12-26 15:17:34 +0200

    added about component and delete option

--- src/app/mock-todo.ts
+++ src/app/mock-todo.ts
@@ -1,14 +1,14 @@
 import {Todo} from "./todo";
 
 export const TODOS: Todo[] = [
-  //new Todo("Wash dishes", 5, 10, "2016-12-25", "15:24"),
-  //new Todo("Walk the dog", 25, 15, "2016-13-12", "23:23"),
-  //new Todo("Get homework done", 41, 40, "2016-12-25", "03:18"),
+  new Todo("Wash dishes", 6, 10, "2016-12-26", "15:24"),
+  new Todo("Walk the dog", 26, 11, "2016-12-26", "23:23"),
+  new Todo("Get homework done", 40, 40, "2016-12-26", "03:18"),
   //new Todo("Get homework done", 11, 40, "2016-12-25", "03:18"),
   //new Todo("Get homework done", 21, 40, "2016-12-25", "03:18"),
   //new Todo("Get homework done", 31, 40, "2016-12-25", "03:18"),
   //new Todo("Get homework done", 41, 20, "2016-12-25", "03:18"),
   //new Todo("Get homework done", 41, 60, "2016-12-25", "03:18"),
-  new Todo("Buy food", 15, 70, "2016-09-18", "17:38")
+  new Todo("Buy food", 7, 40, "2016-12-26", "17:38")
 
 ];
